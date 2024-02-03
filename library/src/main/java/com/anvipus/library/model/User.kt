13ea5f487b2480e95112f21a99fbdcfdcc86eb96
Seno Adji <anvipus.sa@gmail.com>
2024-02-03 16:08:15 +0700

    update

--- library/src/main/java/com/anvipus/library/model/User.kt
+++ library/src/main/java/com/anvipus/library/model/User.kt
@@ -27,3 +27,16 @@ data class User (
 
     val viewAdmin get() = "isAdmin : "+isAdmin
 }
+
+data class Album (
+    @Json(name = "albumId")
+    val albumId: Int?,
+    @Json(name = "id")
+    val id: Int?,
+    @Json(name = "title")
+    val title: String?,
+    @Json(name = "url")
+    val url: String?,
+    @Json(name = "thumbnailUrl")
+    val thumbnailUrl: String?
+) : Parcelable
